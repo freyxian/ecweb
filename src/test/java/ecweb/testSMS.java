345c0047fa184dc5d4ae525ffb596c1f9d193878
jianan <xiaojianan@aol.com>
2016-06-25 02:04:00 +0000

    SMS time out 30 second

--- src/test/java/ecweb/testSMS.java
+++ src/test/java/ecweb/testSMS.java
@@ -2,15 +2,16 @@ package ecweb;
 
 import ecweb.ecoupon.controller.SendSMS;
 import ecweb.ecoupon.util.PassWDGenerate;
+import ecweb.ecoupon.util.SMSException;
 
 public class testSMS {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SMSException {
 		// TODO Auto-generated method stub
 		SendSMS sms = new SendSMS();
 		sms.setHttpUrl("http://api.smsbao.com/sms");
 		sms.setUsername("benyu123");
 		sms.setPassword("benyu123");
-		String messages="喜购云店测试，你的验证码是439809";
+		String messages="【喜购云店】测试，你的验证码是439809";
 		sms.sendSMS("12345678901", messages);
 		sms.sendSMS("13602839344", messages); //朱云峰的国内手机号
 	}
